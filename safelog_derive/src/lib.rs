5bea52ae4a3bf1cef630f6076f11ca62e9d14191
Nick Coish <nick.coish@gmail.com>
2018-08-14 18:33:40 -0400

    temp

--- safelog_derive/src/lib.rs
+++ safelog_derive/src/lib.rs
@@ -1,7 +1,38 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+use syn::DeriveInput;
+
+#[proc_macro_derive(Loggable)]
+pub fn loggable_derive(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap();
+
+    let gen = impl_loggable(&ast);
+
+    //gen.parse().unwrap()
+    gen
 }
+
+fn impl_loggable(ast: &DeriveInput) -> TokenStream {
+    println!("{:#?}", ast);
+    //ast.parse().unwrap();
+    let expanded = quote!{};
+    expanded.into()
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn test() {
+//         #[derive(Loggable)]
+//         struct TestLoggable {
+//             n_i32: i32,
+//             #[safelog(redact)]
+//             n_string: String,
+//         }
+//     }
+// }
